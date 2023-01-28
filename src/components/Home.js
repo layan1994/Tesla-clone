b09519c264b6c9edc8dbfbbda71831a6c9f9db97
Layan Perera <68476361+layan1994@users.noreply.github.com>
2023-01-29 02:26:12 +0530

    added props and props css modification

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,46 @@ import Section from "./Section";
 function Home() {
   return (
     <Container>
-      <Section />
+      <Section
+        title="Model S"
+        description="Order online for touchless delivery"
+        backgroundImage="model-s.jpg"
+        leftBtntext="custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order online for touchless delivery"
+        backgroundImage="model-y.jpg"
+        leftBtntext="custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order online for touchless delivery"
+        backgroundImage="model-x.jpg"
+        leftBtntext="custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels In America"
+        description="Money Back guarantee"
+        backgroundImage="solar-panel.jpg"
+        leftBtntext="custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels In America"
+        description="Money Back guarantee"
+        backgroundImage="solar-roof.jpg"
+        leftBtntext="Order Now"
+        rightBtnText="Learn More"
+      />
+      <Section
+        title="Accessories"
+        backgroundImage="accessories.jpg"
+        leftBtntext="Shop Now"
+      />
     </Container>
   );
 }
